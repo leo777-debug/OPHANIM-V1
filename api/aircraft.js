e2be5c9e8611f9d82bedbfeb63157da399df783a
leo777-debug <leonardvkp@gmail.com>
2026-05-09 09:03:46 +0400

    Update aircraft.js

--- api/aircraft.js
+++ api/aircraft.js
@@ -1,5 +1,10 @@
 export default async function handler(req, res) {
-  const resp = await fetch('https://opensky-network.org/api/states/all?lamin=10&lomin=25&lamax=45&lomax=65');
-  const data = await resp.json();
-  res.json(data);
+  try {
+    const resp = await fetch('https://opensky-network.org/api/states/all?lamin=10&lomin=25&lamax=45&lomax=65');
+    if (!resp.ok) throw new Error('OpenSky returned ' + resp.status);
+    const data = await resp.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
