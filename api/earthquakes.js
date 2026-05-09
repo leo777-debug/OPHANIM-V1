f1baa356e52d171e7e9b3c2b6fe754f26db0428a
leo777-debug <leonardvkp@gmail.com>
2026-05-09 19:10:57 +0400

    Update earthquakes.js

--- api/earthquakes.js
+++ api/earthquakes.js
@@ -1,5 +1,10 @@
 export default async function handler(req, res) {
-  const resp = await fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_week.geojson');
-  const data = await resp.json();
-  res.json(data);
+  try {
+    // all_hour = every earthquake globally past hour, no minimum magnitude!
+    const resp = await fetch('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson');
+    const data = await resp.json();
+    res.json(data);
+  } catch (err) {
+    res.json({ features: [] });
+  }
 }
