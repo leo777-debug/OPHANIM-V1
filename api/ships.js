fc2747c3e5e413f1cec15856fb3d5d5000792572
leo777-debug <leonardvkp@gmail.com>
2026-05-09 09:24:22 +0400

    Delete api/ships.js

--- api/ships.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  const KEY = process.env.AISSTREAM_API_KEY;
-  res.json({ key: KEY ? 'connected' : 'missing', message: 'Use WebSocket for live AIS data' });
-}
