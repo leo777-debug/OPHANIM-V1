eb546dc941cfd6d9868fe32979cbb438c29581de
leo777-debug <leonardvkp@gmail.com>
2026-05-09 12:19:40 +0400

    Update satellites.js

--- api/satellites.js
+++ api/satellites.js
@@ -1,11 +1,13 @@
 export default async function handler(req, res) {
   try {
     const KEY = process.env.N2YO_API_KEY;
-    // Get ALL satellites above MENA center (25°N, 45°E), 60 degree radius
-    const resp = await fetch(`https://api.n2yo.com/rest/v1/satellite/above/25/45/0/60/0/&apiKey=${KEY}`);
+    // Category 52 = Starlink, category 20 = GPS, category 30 = Military
+    const resp = await fetch(`https://api.n2yo.com/rest/v1/satellite/above/25/45/0/70/52/&apiKey=${KEY}`);
     const data = await resp.json();
+    console.log('N2YO response:', JSON.stringify(data));
     res.json(data);
   } catch (err) {
+    console.error('N2YO error:', err.message);
     res.json({ above: [] });
   }
 }
