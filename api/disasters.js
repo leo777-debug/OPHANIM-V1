281743501de02a17cb123e14f16ac8f87a3d23ba
leo777-debug <leonardvkp@gmail.com>
2026-05-09 09:24:42 +0400

    Delete api/disasters.js

--- api/disasters.js
+++
@@ -1,6 +0,0 @@
-export default async function handler(req, res) {
-  const resp = await fetch('https://www.gdacs.org/xml/rss.xml');
-  const text = await resp.text();
-  res.setHeader('Content-Type', 'application/xml');
-  res.send(text);
-}
