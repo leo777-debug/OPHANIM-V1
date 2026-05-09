29b05ce826b38d36f231f2d43777b9595825e431
leo777-debug <leonardvkp@gmail.com>
2026-05-09 09:03:30 +0400

    Update conflicts.js

--- api/conflicts.js
+++ api/conflicts.js
@@ -1,6 +1,9 @@
 export default async function handler(req, res) {
-  // GDELT - real conflict/event data updated every 15 minutes!
-  const resp = await fetch('https://api.gdeltproject.org/api/v2/geo/geo?query=conflict+OR+attack+OR+explosion+OR+missile&TIMESPAN=1440&MAXROWS=25&OUTPUTTYPE=2&FORMAT=JSON');
-  const data = await resp.json();
-  res.json(data);
+  try {
+    const resp = await fetch('https://api.gdeltproject.org/api/v2/geo/geo?query=conflict+OR+attack+OR+explosion+OR+missile&TIMESPAN=1440&MAXROWS=25&OUTPUTTYPE=2&FORMAT=GeoJSON');
+    const data = await resp.json();
+    res.json(data);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
