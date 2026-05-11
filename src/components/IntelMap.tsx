352e8e76b5fa054421fa4148555ef6c36b8b929c
leo777-debug <leonardvkp@gmail.com>
2026-05-11 05:47:15 +0400

    Update IntelMap.tsx

--- src/components/IntelMap.tsx
+++ src/components/IntelMap.tsx
@@ -103,8 +103,8 @@ interface IntelMapProps {
   events: IntelligenceEvent[];
   selectedEvent: IntelligenceEvent | null;
   onEventClick: (event: IntelligenceEvent) => void;
-  showSatellites: boolean;
-  onToggleSatellites: () => void;
+  showSatellites?: boolean;
+  onToggleSatellites?: () => void;
 }
 
 export default function IntelMap({ events, selectedEvent, onEventClick }: IntelMapProps) {
