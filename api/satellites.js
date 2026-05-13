837e61fbb5c20485f4bd1387bb27c0c4ecf25b25
leo777-debug <leonardvkp@gmail.com>
2026-05-13 06:14:11 +0400

    Update satellites.js

--- api/satellites.js
+++ api/satellites.js
@@ -1,3 +1,5 @@
+res.setHeader('Access-Control-Allow-Origin', '*');
+res.setHeader('Cache-Control', 'no-store');
 export default async function handler(req, res) {
   try {
     const KEY = process.env.N2YO_API_KEY;
