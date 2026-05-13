76b60f2723fefbe422f1092932f4651605ad2a2a
leo777-debug <leonardvkp@gmail.com>
2026-05-13 08:11:48 +0400

    Update firms.js

--- api/firms.js
+++ api/firms.js
@@ -1,5 +1,3 @@
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Cache-Control', 'no-store');
 export default async function handler(req, res) {
   const KEY = process.env.NASA_API_KEY;
   const resp = await fetch(`https://firms.modaps.eosdis.nasa.gov/api/area/csv/${KEY}/VIIRS_SNPP_NRT/world/1`);
