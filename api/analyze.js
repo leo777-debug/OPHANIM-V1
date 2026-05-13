4abed36fc413f914fe23d51c707f67b5ce5237e6
leo777-debug <leonardvkp@gmail.com>
2026-05-13 08:09:54 +0400

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -1,5 +1,3 @@
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Cache-Control', 'no-store');
 export default async function handler(req, res) {
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method not allowed' });
   
