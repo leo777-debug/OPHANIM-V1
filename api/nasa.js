71b0493868def521f537697920bb4cf97c3c68af
leo777-debug <leonardvkp@gmail.com>
2026-05-13 08:12:32 +0400

    Remove CORS and cache control headers
    Removed CORS and cache control headers from response.

--- api/nasa.js
+++ api/nasa.js
@@ -1,5 +1,3 @@
-res.setHeader('Access-Control-Allow-Origin', '*');
-res.setHeader('Cache-Control', 'no-store');
 export default async function handler(req, res) {
   try {
     const resp = await fetch('https://eonet.gsfc.nasa.gov/api/v3/events?status=open&limit=10');
